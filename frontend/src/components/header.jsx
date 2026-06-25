832c1ed9132b1e3f3607f876bf64d46c8b5c9e29
Pranav Garg <gargpranav2112@gmail.com>
2026-06-25 00:25:20 -0400

    Clean up imports in header.jsx
    Removed unused imports from header component.

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../styles/header.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 class Header extends Component {
